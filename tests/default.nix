5d27da2c3470663613b4f9307f87fd904cda8f61
Proteus Qian <sudaku233@outlook.com>
2026-05-22 03:59:22 +0800

    🔥 refactor(tests): remove bind DNS server tests
    - remove `tests/bind.nix` test module as the custom declarative bind configuration was integrated into my Nixpkgs fork
    - remove `bind` test entry from `tests/default.nix`
    
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

--- tests/default.nix
+++ tests/default.nix
@@ -3,9 +3,7 @@
   # - To enter interactive environment, run `nix run .#checks.x86_64-linux.bind.driverInteractive`
   # - To get a shell, either use `interactive.sshBackdoor.enable = true;` or `<machine_name>.shell_interact()` from the
   #   Python REPL
-
   traffic-quota = pkgs.callPackage ./traffic-quota.nix {};
-  bind = pkgs.callPackage ./bind.nix {};
 
   # nix-darwin does not have a VM testing framework
   # easytier = pkgs.callPackage ./easytier.nix {};
