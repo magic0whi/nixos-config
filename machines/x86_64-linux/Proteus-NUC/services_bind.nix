1601fba183756693dfa0ecd091884766679dcde3
Proteus Qian <sudaku233@outlook.com>
2026-05-23 16:10:30 +0800

    refactor(NUC::Services::Bind): Update Server Certificate to add apex domain as SAN
    - Comment `services.bind.directory` since I plan to secret manage DNSSEC keys
    - reorder trusted island related settings down
    
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

--- machines/x86_64-linux/Proteus-NUC/services_bind.nix
+++ machines/x86_64-linux/Proteus-NUC/services_bind.nix
@@ -8,47 +8,11 @@
     allowedTCPPorts = [53 853];
     allowedUDPPorts = [53]; # Bind don't support DNS-over QUIC
   };
-  services.resolved.settings.Resolve = {
-    DNSSEC = "allow-downgrade";
-    Domains =
-      [
-        "~${myvars.domain}" # The '~' prefix makes this a routing domain
-      ]
-      ++ (lib.mapAttrsToList (_: zone:
-        if (lib.isDerivation zone.file)
-        then "~${zone.file.name}"
-        else "~${zone.file}")
-      config.services.bind.zones);
-
-    DNS =
-      (lib.concatMap (iface: lib.optional (iface ? ipv4) "${iface.ipv4}#${myvars.domain}")
-        myvars.networking.hosts_addr.${config.networking.hostName})
-      ++ (lib.concatMap (iface: lib.optional (iface ? ipv6) "${iface.ipv6}#${myvars.domain}")
-        myvars.networking.hosts_addr.${config.networking.hostName});
-  };
-  # Trust Island
-  # NOTE: Query the zone apex (`proteus.eu.org`, `161.64.100.in-addr.arpa`
-  # `0.e.1.a.c.5.1.1.a.7.d.f.ip6.arpa`)
-  # nix run nixpkgs#dig -- @100.64.161.20 161.64.100.in-addr.arpa DNSKEY +noall +answer | nix shell nixpkgs#bind --command dnssec-dsfromkey -f - 161.64.100.in-addr.arpa
-  # Or
-  # nix run nixpkgs#dig -- @100.64.161.20 161.64.100.in-addr.arpa DNSKEY +noall +answer | nix shell nixpkgs#ldns.examples --command ldns-key2ds -n /dev/stdin
-  environment.etc."dnssec-trust-anchors.d/${myvars.domain}.positive".text = ''
-    ${myvars.domain}. IN DS 19905 15 2 AC53E45BD2ECD7E4D8DED050FB08E0F37095AF97E0B6F73CE912A56CE5C542C0
-  '';
-  # environment.etc."dnssec-trust-anchors.d/${lib.removeSuffix ".zone" reverse_v4_zones."100".name}.positive".text = ''
-  #   ${lib.removeSuffix ".zone" reverse_v4_zones."100".name}. IN DS 32237 15 2 5F089BE41C87322212B05BAB4A760097235220F5346A1F51F6161728B77A0F8F
-  # '';
-  # environment.etc."dnssec-trust-anchors.d/${lib.removeSuffix ".zone" reverse_v4_zones."192".name}.positive".text = ''
-  #   ${lib.removeSuffix ".zone" reverse_v4_zones."192".name}. IN DS 25153 15 2 B5B5AC75FDCC85AACBDF747323AC5F7CA8D8FC482D03C848DEE4EFAD79F7CD50
-  # '';
-  # environment.etc."dnssec-trust-anchors.d/${lib.removeSuffix ".zone" reverse_v6_zones_ts."0.e.1.a.c.5.1.1.a.7.d.f".name}.positive".text = ''
-  #   ${lib.removeSuffix ".zone" reverse_v6_zones_ts."0.e.1.a.c.5.1.1.a.7.d.f".name}. IN DS 60960 15 2 DD09A9E95F7C7851FAC65FD39FDE55FAB2C001D5B37D744F98AA23C56FD63D16
-  # '';
   services.bind = {
     enable = true;
-    # Persistent directory for DNSSEC key states.
-    # NixOS defaults to /run/named, which clears on reboot.
-    directory = "/srv/bind";
+    # Persistent directory for DNSSEC key states. Defaults to "/run/named", which clears on reboot.
+    # directory = "/srv/bind";
+
     # Access-control of what networks are allowed for recursive queries
     # cacheNetworks = [
     #   "127.0.0.0/8" "::1/128"
@@ -113,7 +77,7 @@
         mutable = true;
         soa = {
           rName = myvars.useremail;
-          serial = "2026052300";
+          serial = "2026052301";
         };
         networks = [
           {
@@ -127,11 +91,7 @@
         ];
         hosts = let
           allowed_hosts =
-            [
-              "Proteus-NUC"
-              "Proteus-MBP14M4P"
-              "Proteus-Desktop"
-            ]
+            ["Proteus-NUC" "Proteus-MBP14M4P" "Proteus-Desktop"]
             ++ map (i: "Proteus-NixOS-${toString i}") (lib.range 0 5);
         in
           lib.mapAttrs (_: ifaces:
@@ -149,7 +109,7 @@
           # Intercept standard DoH queries at the apex domain
           rule = lib.concatStrings [
             "("
-            "Host(`${myvars.domain}`)" # TODO: Current server cert lacks SAN for apex domain
+            "Host(`${myvars.domain}`)"
             " || Host(`ns1.${myvars.domain}`)"
             " || Host(`${config.networking.hostName}.${myvars.tailnet}`)"
             ")"
@@ -181,4 +141,42 @@
       };
     };
   };
+
+  services.resolved.settings.Resolve = {
+    DNSSEC = "allow-downgrade";
+    Domains =
+      [
+        "~${myvars.domain}" # The '~' prefix makes this a routing domain
+      ]
+      ++ (lib.mapAttrsToList (_: zone:
+        if (lib.isDerivation zone.file)
+        then "~${zone.file.name}"
+        else "~${zone.file}")
+      config.services.bind.zones);
+
+    DNS =
+      (lib.concatMap (iface: lib.optional (iface ? ipv4) "${iface.ipv4}#${myvars.domain}")
+        myvars.networking.hosts_addr.${config.networking.hostName})
+      ++ (lib.concatMap (iface: lib.optional (iface ? ipv6) "${iface.ipv6}#${myvars.domain}")
+        myvars.networking.hosts_addr.${config.networking.hostName});
+  };
+
+  # Trust Island
+  # NOTE: Query the zone apex (`proteus.eu.org`, `161.64.100.in-addr.arpa`
+  # `0.e.1.a.c.5.1.1.a.7.d.f.ip6.arpa`)
+  # nix run nixpkgs#dig -- @100.64.161.20 161.64.100.in-addr.arpa DNSKEY +noall +answer | nix shell nixpkgs#bind --command dnssec-dsfromkey -f - 161.64.100.in-addr.arpa
+  # Or
+  # nix run nixpkgs#dig -- @100.64.161.20 161.64.100.in-addr.arpa DNSKEY +noall +answer | nix shell nixpkgs#ldns.examples --command ldns-key2ds -n /dev/stdin
+  environment.etc."dnssec-trust-anchors.d/${myvars.domain}.positive".text = ''
+    ${myvars.domain}. IN DS 19905 15 2 AC53E45BD2ECD7E4D8DED050FB08E0F37095AF97E0B6F73CE912A56CE5C542C0
+  '';
+  # environment.etc."dnssec-trust-anchors.d/${lib.removeSuffix ".zone" reverse_v4_zones."100".name}.positive".text = ''
+  #   ${lib.removeSuffix ".zone" reverse_v4_zones."100".name}. IN DS 32237 15 2 5F089BE41C87322212B05BAB4A760097235220F5346A1F51F6161728B77A0F8F
+  # '';
+  # environment.etc."dnssec-trust-anchors.d/${lib.removeSuffix ".zone" reverse_v4_zones."192".name}.positive".text = ''
+  #   ${lib.removeSuffix ".zone" reverse_v4_zones."192".name}. IN DS 25153 15 2 B5B5AC75FDCC85AACBDF747323AC5F7CA8D8FC482D03C848DEE4EFAD79F7CD50
+  # '';
+  # environment.etc."dnssec-trust-anchors.d/${lib.removeSuffix ".zone" reverse_v6_zones_ts."0.e.1.a.c.5.1.1.a.7.d.f".name}.positive".text = ''
+  #   ${lib.removeSuffix ".zone" reverse_v6_zones_ts."0.e.1.a.c.5.1.1.a.7.d.f".name}. IN DS 60960 15 2 DD09A9E95F7C7851FAC65FD39FDE55FAB2C001D5B37D744F98AA23C56FD63D16
+  # '';
 }
