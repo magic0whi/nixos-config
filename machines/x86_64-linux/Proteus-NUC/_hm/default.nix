d759dd1a7be38ec8c5bdbfb50b6b1cb40210e1c1
Proteus Qian <sudaku233@outlook.com>
2026-05-23 14:38:27 +0800

    refactor(NUC::HM::Cloud-Providers): Split to standaline nix file
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

--- machines/x86_64-linux/Proteus-NUC/_hm/default.nix
+++ machines/x86_64-linux/Proteus-NUC/_hm/default.nix
@@ -1,7 +1,6 @@
 {
   config,
   mylib,
-  myvars,
   pkgs,
   ...
 }: {
@@ -15,50 +14,9 @@
     qpdf
     act # Run your GitHub Actions locally
     gemini-cli
-    google-cloud-sdk # gcloud
-    terraform
-    terraformer
-    terraform-ls # LSP
     witr
   ];
   ## END packages.nix
-  ## BEGIN cloud-providers.nix
-  sops.secrets = {
-    "project-0.secret.json" = {
-      sopsFile = "${myvars.secrets_dir}/gcloud_project-0.secret.json.sops";
-      format = "binary";
-      path = "${config.xdg.configHome}/gcloud/project-0.secret.json";
-    };
-    "project-1.secret.json" = {
-      sopsFile = "${myvars.secrets_dir}/gcloud_project-1.secret.json.sops";
-      format = "binary";
-      path = "${config.xdg.configHome}/gcloud/project-1.secret.json";
-    };
-  };
-  # Add plugin terraform-provider-google for `terraformer`
-  home.file = let
-    arch = "linux_amd64";
-    version = "7.31.0";
-    provider = pkgs.terraform-providers.hashicorp_google.overrideAttrs (_: {
-      inherit version;
-      src = pkgs.fetchFromGitHub {
-        owner = "hashicorp";
-        repo = "terraform-provider-google";
-        rev = "v${version}";
-        hash = "sha256-6cvRvVQmKRi4kyNAo/UAGN00bO+uCJYvf661xYW/QCQ=";
-      };
-      vendorHash = "sha256-UoS4iIVHhCQ+Zk+SJmsMHJgJBKLMbfMVmtm4MDmzT68=";
-      postInstall = ''
-        dir=$out/libexec/terraform-providers/registry.terraform.io/hashicorp/google/${version}/''${GOOS}_''${GOARCH}
-        mkdir -p "$dir"
-        mv $out/bin/* "$dir/terraform-provider-google_${version}"
-        rmdir $out/bin
-      '';
-    });
-  in {
-    ".terraform.d/plugins/${arch}/terraform-provider-google_v${version}".source = "${provider}/libexec/terraform-providers/registry.terraform.io/hashicorp/google/${version}/${arch}/terraform-provider-google_${version}";
-  };
-  ## END cloud-providers.nix
   # programs.ssh = {
   #   enable = true;
   #   enableDefaultConfig = false;
---
+++ machines/x86_64-linux/Proteus-NUC/cloud-providers.nix
@@ -0,0 +1,48 @@
+{
+  config,
+  myvars,
+  pkgs,
+  ...
+}: {
+  sops.secrets = {
+    "project-0.secret.json" = {
+      sopsFile = "${myvars.secrets_dir}/gcloud_project-0.secret.json.sops";
+      format = "binary";
+      path = "${config.xdg.configHome}/gcloud/project-0.secret.json";
+    };
+    "project-1.secret.json" = {
+      sopsFile = "${myvars.secrets_dir}/gcloud_project-1.secret.json.sops";
+      format = "binary";
+      path = "${config.xdg.configHome}/gcloud/project-1.secret.json";
+    };
+  };
+  home.packages = with pkgs; [
+    google-cloud-sdk # gcloud
+    terraform
+    terraformer
+    terraform-ls # LSP
+  ];
+  # Add plugin terraform-provider-google for `terraformer`
+  home.file = let
+    arch = "linux_amd64";
+    version = "7.31.0";
+    provider = pkgs.terraform-providers.hashicorp_google.overrideAttrs (_: {
+      inherit version;
+      src = pkgs.fetchFromGitHub {
+        owner = "hashicorp";
+        repo = "terraform-provider-google";
+        rev = "v${version}";
+        hash = "sha256-6cvRvVQmKRi4kyNAo/UAGN00bO+uCJYvf661xYW/QCQ=";
+      };
+      vendorHash = "sha256-UoS4iIVHhCQ+Zk+SJmsMHJgJBKLMbfMVmtm4MDmzT68=";
+      postInstall = ''
+        dir=$out/libexec/terraform-providers/registry.terraform.io/hashicorp/google/${version}/''${GOOS}_''${GOARCH}
+        mkdir -p "$dir"
+        mv $out/bin/* "$dir/terraform-provider-google_${version}"
+        rmdir $out/bin
+      '';
+    });
+  in {
+    ".terraform.d/plugins/${arch}/terraform-provider-google_v${version}".source = "${provider}/libexec/terraform-providers/registry.terraform.io/hashicorp/google/${version}/${arch}/terraform-provider-google_${version}";
+  };
+}
