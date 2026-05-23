acf3439577ce59fbc3ee454d366e2670a6e87b5c
Proteus Qian <sudaku233@outlook.com>
2026-05-23 12:19:51 +0800

    feat(NUC:Services:Navidrom): Enable Forward Auth for Navidrom
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

--- machines/x86_64-linux/Proteus-NUC/services_navidrome.nix
+++ machines/x86_64-linux/Proteus-NUC/services_navidrome.nix
@@ -5,14 +5,30 @@
 }: {
   services.navidrome = {
     enable = true;
-    settings.MusicFolder = "${config.users.users.${myvars.username}.home}/Music";
+    settings = {
+      MusicFolder = "${config.users.users.${myvars.username}.home}/Music";
+      ExtAuth = {
+        TrustedSources = "127.0.0.1/32,::1/128";
+        LogoutURL = "https://auth.${myvars.domain}/logout";
+      };
+    };
   };
   services.traefik.dynamicConfigOptions.http = {
-    routers.navidrome = {
-      rule = "Host(`navidrome.${myvars.domain}`)";
-      entryPoints = ["websecure"];
-      service = "navidrome";
-      tls = {};
+    routers = {
+      navidrome = {
+        rule = "Host(`navidrome.${myvars.domain}`)";
+        entryPoints = ["websecure"];
+        middlewares = ["authelia-auth"];
+        service = "navidrome";
+        tls = {};
+      };
+      # Authentication bypass for share and subsonic endpoints
+      navidrome-public = {
+        rule = "Host(`navidrome.${myvars.domain}`) && (PathPrefix(`/share/`) || PathPrefix(`/rest/`))";
+        entryPoints = ["websecure"];
+        service = "navidrome";
+        tls = {};
+      };
     };
     services.navidrome.loadBalancer.servers = [
       (with config.services.navidrome.settings; {url = "http://${Address}:${toString Port}";})
