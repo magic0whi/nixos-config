b219e828027a4be643daf495c37fc5300ad1550b
Proteus Qian <sudaku233@outlook.com>
2026-05-23 14:39:11 +0800

    refactor(NUC::HM): Split packages.nix and miscs.nix to standalone nix files
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

---
+++ machines/x86_64-linux/Proteus-NUC/_hm/packages.nix
@@ -0,0 +1,12 @@
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    (nvtopPackages.intel.override {nvidia = true;})
+    minicom # embedded development
+    chezmoi
+    libreoffice
+    qpdf
+    act # Run your GitHub Actions locally
+    gemini-cli
+    witr
+  ];
+}
