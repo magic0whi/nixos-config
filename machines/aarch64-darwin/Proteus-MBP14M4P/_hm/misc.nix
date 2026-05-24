b1510effcd1138d60696e1ab9efbafbf41686dc0
Proteus Qian <sudaku233@outlook.com>
2026-05-24 14:42:07 +0800

    ♻️ refactor(MBP14M4P::HM): slim down default.nix and split content into misc.nix and packages.nix
    - reduce default.nix to a single-line scan_path import
    - extract aerospace and mpv config into new misc.nix
    - extract home.packages into new packages.nix
    
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

---
+++ machines/aarch64-darwin/Proteus-MBP14M4P/_hm/misc.nix
@@ -0,0 +1,16 @@
+_: {
+  ## BEGIN aerospace.nix
+  programs.aerospace.settings.workspace-to-monitor-force-assignment = {
+    "7" = ["C340SCA"];
+    "8" = ["C340SCA"];
+    "9" = ["RTK UHD HDR"];
+    "0" = ["RTK UHD HDR"];
+  };
+  ## END aerospace.nix
+  ## BEGIN mpv.nix
+  programs.mpv.profiles.common = {
+    vulkan-device = "Apple M4 Pro";
+    ao = "avfoundation";
+  };
+  ## END mpv.nix
+}
