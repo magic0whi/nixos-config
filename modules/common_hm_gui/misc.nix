5e99f590fa1e6410c8991daa26e7ebe23452b992
Proteus Qian <sudaku233@outlook.com>
2026-05-27 15:27:48 +0800

    ♻️ refactor(common_hm_gui::misc): move `google-chrome` config to common GUI module with Darwin guard
    - relocate `programs.google-chrome` from `nixos_hm_gui/misc.nix` to `common_hm_gui/misc.nix`
    - wrap with `lib.mkIf (!pkgs.stdenv.isDarwin)` so it is skipped on macOS
    
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

--- modules/common_hm_gui/misc.nix
+++ modules/common_hm_gui/misc.nix
@@ -1,6 +1,16 @@
 { lib, pkgs, ... }:
 {
   ## BEGIN browsers.nix
+  # https://github.com/nix-community/home-manager/blob/master/modules/programs/chromium.nix
+  programs.google-chrome = lib.mkIf (!pkgs.stdenv.isDarwin) {
+    enable = true;
+    # https://wiki.archlinux.org/title/Chromium#Native_Wayland_support
+    commandLineArgs = [
+      "--ozone-platform-hint=auto"
+      "--enable-wayland-ime" # Make it use text-input-v1, which works for kwin 5.27 and weston
+      # "--enable-features=Vulkan" # Enable hardware acceleration - vulkan api
+    ];
+  };
   programs.firefox.enable = true;
   ## END browsers.nix
   ## BEGIN vscode.nix
