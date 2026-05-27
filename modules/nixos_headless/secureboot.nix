1d22f744703a40b18ad69faa0ce3cde22fc3eea7
Proteus Qian <sudaku233@outlook.com>
2026-05-27 11:58:22 +0800

    ✨ feat(NixOS::Headless::SecureBoot): migrate to shared module with sops-managed PKI keys
    - move secureboot config from `machines/x86_64-linux/Proteus-NUC/secureboot.nix` to `modules/nixos_headless/secureboot.nix`
    - make secureboot keys declerative, add sops secrets for PK, KEK, and db private keys, placing them directly into lanzaboote's pkiBundle key paths
    - add systemd tmpfiles rules to symlink PEM certificates into the corresponding pkiBundle directories
    - commit PKI certificates: `secureboot_PK.pem`, `secureboot_KEK.pem`, `secureboot_db.pem`
    - add SOPS-encrypted key files: `secureboot_PK.key.sops`, `secureboot_KEK.key.sops`, `secureboot_db.key.sops`
    - register creation rules for the three new secret files in `.sops.yaml`
    - update `.sops.yaml` comment to prefix with `TIP:`
    - bump sops version 3.12.2 → 3.13.1 in `secrets/common.sops.yaml`
    
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

---
+++ modules/nixos_headless/secureboot.nix
@@ -0,0 +1,44 @@
+{
+  config,
+  myvars,
+  pkgs,
+  ...
+}:
+{
+  sops.secrets = {
+    "secureboot_PK.key" = {
+      sopsFile = "${myvars.secrets_dir}/secureboot_PK.key.sops";
+      path = "${config.boot.lanzaboote.pkiBundle}/keys/PK/PK.key";
+      format = "binary";
+    };
+    "secureboot_KEK.key" = {
+      sopsFile = "${myvars.secrets_dir}/secureboot_KEK.key.sops";
+      path = "${config.boot.lanzaboote.pkiBundle}/keys/KEK/KEK.key";
+      format = "binary";
+    };
+    "secureboot_db.key" = {
+      sopsFile = "${myvars.secrets_dir}/secureboot_db.key.sops";
+      path = "${config.boot.lanzaboote.pkiBundle}/keys/db/db.key";
+      format = "binary";
+    };
+  };
+  systemd.tmpfiles.settings = {
+    "01-secureboot-PK-key"."${config.boot.lanzaboote.pkiBundle}/keys/PK/PK.pem"."L+".argument =
+      "${myvars.secrets_dir}/secureboot_PK.pem";
+    "01-secureboot-KEK-key"."${config.boot.lanzaboote.pkiBundle}/keys/KEK/KEK.pem"."L+".argument =
+      "${myvars.secrets_dir}/secureboot_KEK.pem";
+    "01-secureboot-db-key"."${config.boot.lanzaboote.pkiBundle}/keys/db/db.pem"."L+".argument =
+      "${myvars.secrets_dir}/secureboot_db.pem";
+  };
+
+  environment.systemPackages = [ pkgs.sbctl ]; # For debugging and troubleshooting Secure Boot.
+  # Lanzaboote currently replaces the systemd-boot module.
+  # This setting is usually set to true in configuration.nix generated at installation time. So we force it to false for now.
+  boot.loader.systemd-boot.enable = false;
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/var/lib/sbctl"; # sudo sbctl create-keys
+    # allowUnsigned = true; # Useful for first boot
+    # autoEnrollKeys.enable = true;
+  };
+}
