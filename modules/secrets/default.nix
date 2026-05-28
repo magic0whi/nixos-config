9b053caa2cd97e34a853e9a4579d4d5ca710c0b2
Proteus Qian <sudaku233@outlook.com>
2026-05-29 03:44:06 +0800

    fix(modules::secrets): Revert: use persistent ssh key path when impermanence is active
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

--- modules/secrets/default.nix
+++ modules/secrets/default.nix
@@ -1,6 +1,16 @@
-{ myvars, pkgs, ... }:
+{
+  config,
+  myvars,
+  pkgs,
+  ...
+}:
 {
   environment.systemPackages = [ pkgs.sops ];
   sops.defaultSopsFile = "${myvars.secretsDir}/common.sops.yaml";
-  # sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  # sops-nix starts very early (before impermanence mount)
+  sops.age.sshKeyPaths =
+    if (config.environment ? persistence && config.environment.persistence != { }) then
+      [ "/persistent/etc/ssh/ssh_host_ed25519_key" ]
+    else
+      [ "/etc/ssh/ssh_host_ed25519_key" ];
 }
