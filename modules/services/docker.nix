3f5100031ce8c896d3f6de349539a171a99ab55d
Proteus Qian <sudaku233@outlook.com>
2026-05-30 19:32:37 +0800

    fix(Services::Docker): fix actions fail
    copyContent: failed to copy content to container: Error response from daemon: mkdirat var/run/act: path escapes from parent
    
    ref: https://github.com/nektos/act/issues/6092
    
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

--- modules/services/docker.nix
+++ modules/services/docker.nix
@@ -67,6 +67,14 @@
 
   virtualisation.docker = {
     enable = true;
+    package = pkgs.docker_29.override {
+      version = "29.5.2";
+      cliRev = "v29.5.2";
+      mobyRev = "docker-v29.5.2";
+
+      cliHash = "sha256-kHgDZVr6mAyCtZ6bSG9FWV0GhWDfXLXzHYFrmjFzO9w=";
+      mobyHash = "sha256-lux7tTyF6vm5wuIXs+z3Ygd2v4JjgHbRvOXNA4kjNtg=";
+    };
     # storageDriver = "btrfs"; # conflict with feature: containerd-snapshotter
     daemon.settings = {
       firewall-backend = "nftables"; # Requires >= docker 29
