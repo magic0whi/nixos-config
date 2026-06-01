156c4d4ddf58c25e8853065759bdb589c2a86df5
Proteus Qian <sudaku233@outlook.com>
2026-06-01 17:55:37 +0800

    feat(NixOS::Headless::SSSD): extract `sssd.nix` from `NixOS::Headless` to a dedicated module
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

---
+++ modules/nixos_headless/sssd.nix
@@ -0,0 +1,52 @@
+{ config, myvars, ... }:
+{
+  system.nssDatabases.sudoers = [ "sss" ]; # Use LDAP to distribute configuration of sudo as well
+  sops =
+    let
+      restartUnits = [ "sssd.service" ];
+      sopsFile = "${myvars.secretsDir}/common.sops.yaml";
+    in
+    {
+      secrets."sssd_ldap_default_authtok" = { inherit sopsFile restartUnits; };
+      templates."sssd.env" = {
+        # https://github.com/NixOS/nixpkgs/blob/15f4ee454b1dce334612fa6843b3e05cf546efab/nixos/modules/services/misc/sssd.nix#L111-L113
+        inherit restartUnits;
+        content = "SSSD_LDAP_DEFAULT_AUTHTOK='${config.sops.placeholder.sssd_ldap_default_authtok}'";
+      };
+    };
+  services.sssd = {
+    enable = true;
+    environmentFile = config.sops.templates."sssd.env".path;
+    settings = {
+      sssd = {
+        # debug_level = 7;
+        services = "ifp, nss, pam, sudo";
+        domains = "LDAP";
+      };
+      # "pam".pam_verbosity = 3;
+      "domain/LDAP" =
+        let
+          base_dn = "dc=" + builtins.replaceStrings [ "." ] [ ",dc=" ] myvars.domain;
+        in
+        {
+          override_shell = "/run/current-system/sw/bin/${config.users.defaultUserShell.meta.mainProgram}";
+          cache_credentials = true;
+          entry_cache_timeout = 600;
+          enumerate = true;
+
+          id_provider = "ldap";
+          auth_provider = "ldap";
+          chpass_provider = "ldap";
+
+          ldap_uri = "ldaps://ldap.${myvars.domain}:636";
+          ldap_default_bind_dn = "uid=sssd,ou=ServiceAccounts,${base_dn}";
+          ldap_default_authtok = "$SSSD_LDAP_DEFAULT_AUTHTOK";
+          ldap_search_base = base_dn;
+          ldap_sudo_search_base = "ou=Sudoers,${base_dn}";
+          ldap_tls_reqcert = "demand";
+          ldap_network_timeout = 2;
+          ldap_schema = "rfc2307bis";
+        };
+    };
+  };
+}
