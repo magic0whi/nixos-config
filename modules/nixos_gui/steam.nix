d13c0f5d10b8e3cc95bc9055cb8ffee7669f97e3
Proteus Qian <sudaku233@outlook.com>
2026-06-05 21:41:39 +0800

    chore(NixOS::GUI): remove unnecessary guards
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

--- modules/nixos_gui/steam.nix
+++ modules/nixos_gui/steam.nix
@@ -1,13 +1,12 @@
 {
-  config,
   pkgs,
   ...
 }:
 {
   programs.steam = {
-    enable = pkgs.stdenv.isx86_64;
+    enable = true;
     # This also enables programs.gamescope.enable
-    gamescopeSession.enable = config.programs.steam.enable;
+    gamescopeSession.enable = true;
     # Packages only available to Steam
     # Use `steam-run` to run Steam FHS, e.g. `steam-run gamescope`
     extraPackages = with pkgs; [ gamescope ];
