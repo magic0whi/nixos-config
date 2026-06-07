c3a29ccbbea39918e6ef589a65127009915c1bab
Proteus Qian <sudaku233@outlook.com>
2026-06-07 11:29:53 +0800

    refactor(Desktop::Services::Authelia): centralize nixos-search's OIDC client
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

--- machines/x86_64-linux/Proteus-Desktop/services/authelia.nix
+++ machines/x86_64-linux/Proteus-Desktop/services/authelia.nix
@@ -301,6 +301,19 @@ in
             access_token_signed_response_alg = "RS256";
             audience = [ "niks3" ]; # Whitelist the requested audience
           }
+          {
+            client_id = "nixos-search";
+            client_name = "NixOS Search";
+            client_secret = "$pbkdf2-sha512$310000$ABGWSADqYSUeeF5ZDQlhNg$c4aee7uOMUwHOHv5myhhu.VNxwEBhBnNUAI1DjutkmICQ33W1QnJA0k8cQkpEOubCa5jGrFC.e6Un4QuKnu0YQ";
+            redirect_uris = [ "https://nixos-search.${myvars.domain}/auth/callback" ];
+            scopes = [
+              "openid"
+              "profile"
+              "email"
+              "groups"
+            ];
+            token_endpoint_auth_method = "client_secret_post";
+          }
         ];
       };
     };
--- machines/x86_64-linux/Proteus-Desktop/services/nixos-search.nix
+++ machines/x86_64-linux/Proteus-Desktop/services/nixos-search.nix
@@ -299,19 +299,4 @@ in
         '')
       ];
     };
-  services.authelia.instances.main.settings.identity_providers.oidc.clients = [
-    {
-      client_id = "nixos-search";
-      client_name = "NixOS Search";
-      client_secret = "$pbkdf2-sha512$310000$ABGWSADqYSUeeF5ZDQlhNg$c4aee7uOMUwHOHv5myhhu.VNxwEBhBnNUAI1DjutkmICQ33W1QnJA0k8cQkpEOubCa5jGrFC.e6Un4QuKnu0YQ";
-      redirect_uris = [ "https://nixos-search.${myvars.domain}/auth/callback" ];
-      scopes = [
-        "openid"
-        "profile"
-        "email"
-        "groups"
-      ];
-      token_endpoint_auth_method = "client_secret_post";
-    }
-  ];
 }
