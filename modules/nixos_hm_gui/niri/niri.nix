e44ae58959f75a1ae33584457cebb1940ade6a2c
Proteus Qian <sudaku233@outlook.com>
2026-06-15 23:14:18 +0800

    ♻️ refactor(niri): remove unused xwayland-satellite package
    as I implement it in niri-nix
    
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

--- modules/nixos_hm_gui/niri/niri.nix
+++ modules/nixos_hm_gui/niri/niri.nix
@@ -1,12 +1,5 @@
 { pkgs, ... }:
 {
-  # TODO test if required
-  # home.packages = with pkgs; [
-  #   # Niri v25.08 will create X11 sockets on disk, export $DISPLAY, and spawn `xwayland-satellite` on-demand when an X11 client connects
-  #   xwayland-satellite
-  #   # For Screenshot Annotation
-  # ];
-
   # NOTE: this executable is used by greetd to start a wayland session when system boot up with such a
   # vendor-no-locking script, we can switch to another wayland compositor without modifying greetd's config in NixOS
   # module
