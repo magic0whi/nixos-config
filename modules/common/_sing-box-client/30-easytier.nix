b4bdbf41e9cceb671f0486b48d367c56383ca624
Proteus Qian <sudaku233@outlook.com>
2026-06-19 01:07:53 +0800

    feat(sing-box-client::easytier): add geosite-category-stun

--- modules/common/_sing-box-client/30-easytier.nix
+++ modules/common/_sing-box-client/30-easytier.nix
@@ -1,4 +1,9 @@
-{ lib, mylib, ... }:
+{
+  lib,
+  mylib,
+  ruleSetCfg,
+  ...
+}:
 let
   out = "Direct";
   non_dns_rules = [
@@ -13,22 +18,32 @@ let
   rules = [
     {
       domain_suffix = [
-        "stun.miwifi.com"
         "stun.chat.bilibili.com"
+        "stun-heyuan-v6.easytier.cn"
         "public.easytier.cn"
       ];
     }
+    { rule_set = "geosite-category-stun"; }
   ];
 in
 {
   dns.rules = lib.mkBefore (mylib.mkSbRules true out rules);
-  route.rules = lib.mkMerge (
-    let
-      mkSbRules = mylib.mkSbRules false;
-    in
-    [
-      (lib.mkBefore (mkSbRules out non_dns_rules))
-      (lib.mkOrder 875 (mkSbRules out rules))
-    ]
-  );
+  route = {
+    rule_set = lib.singleton (
+      ruleSetCfg.defaultCfg
+      // {
+        tag = "geosite-category-stun";
+        url = "${ruleSetCfg.urlPrefix}/geo/geosite/category-stun.srs";
+      }
+    );
+    rules = lib.mkMerge (
+      let
+        mkSbRules = mylib.mkSbRules false;
+      in
+      [
+        (lib.mkBefore (mkSbRules out non_dns_rules))
+        (lib.mkOrder 875 (mkSbRules out rules))
+      ]
+    );
+  };
 }
