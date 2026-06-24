a1de6c7e1c1c7c2e2f8cdfb5ab0f562720f819e8
Proteus Qian <sudaku233@outlook.com>
2026-06-24 11:57:32 +0800

    feat(Desktop::impermanence): rollback to blank subvolume on startup

--- machines/x86_64-linux/Proteus-Desktop/impermanence.nix
+++ machines/x86_64-linux/Proteus-Desktop/impermanence.nix
@@ -1,4 +1,18 @@
-_: {
+{ config, ... }:
+{
+  # Rollback `/` to blank snapshot on boot
+  boot.initrd.systemd.services."zfs-rollback-root" = {
+    description = "Rollback zroot/root@blank in initrd";
+    wantedBy = [ "zfs-import.target" ];
+    after = [ "zfs-import-zroot.service" ]; # Make sure zroot is imported
+    before = [ "sysroot.mount" ]; # Make sure this happens before root is mounted
+    serviceConfig = {
+      Type = "oneshot";
+      # `-r` destroys any snapshots and bookmarks more recent than the one specified
+      ExecStart = "${config.boot.zfs.package}/sbin/zfs rollback -r zroot/root@blank";
+    };
+  };
+
   environment.persistence."/persistent".files = [
     {
       # TODO: Unsafe
