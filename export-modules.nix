62fc32df41a569387ab9e47eb5fa2c9dbcfad9bc
Proteus Qian <sudaku233@outlook.com>
2026-06-27 18:09:58 +0800

    chore(flake): minor fixes

--- export-modules.nix
+++ export-modules.nix
@@ -4,5 +4,6 @@
     # lib.modules.importApply ./modules/to/export.nix { mylib = ...; };
     nixosModules.traffix-quota = ./modules/nixos_headless/traffic-quota.nix;
     homeModules.niri = ./modules/nixos_hm_gui/niri/niri.nix;
+    # TODO: export sing-box-subscribe
   };
 }
