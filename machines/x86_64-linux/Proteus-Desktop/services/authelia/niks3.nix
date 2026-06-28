45c5c2a9557ddf1e81925f53a8dccd1e8122262e
Proteus Qian <sudaku233@outlook.com>
2026-06-28 15:21:44 +0800

    chore(authelia/niks3): rename client name

--- machines/x86_64-linux/Proteus-Desktop/services/authelia/niks3.nix
+++ machines/x86_64-linux/Proteus-Desktop/services/authelia/niks3.nix
@@ -2,7 +2,7 @@
 {
   services.authelia.instances.main.settings.identity_providers.oidc.clients = lib.singleton {
     client_id = "niks3_yajuusexnpai";
-    client_name = "Niks3 Client Credentials Test";
+    client_name = "Niks3 Client Credentials";
     client_secret = "$pbkdf2-sha512$310000$3oNLFhvo3pg1e/YTke0dQw$VOx0ZqszL4IFhY5bMaeEwQOicTq4.egK3HaPrxG5BRoQfv.FPT35EKZW9ZPkEtj4LYy2wJR2BQM3DCdXxRLaBg";
     grant_types = [ "client_credentials" ];
     # Niks3 requires a signed JWT, force Authelia to issue a signed JWT access token instead of access token only
